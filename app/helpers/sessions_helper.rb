8fe99d171d6d4964e80313ea4be2e510a1b461e6
hvdung14 <hvdung14@gmail.com>
2019-01-02 14:02:32 +0700

    to Chapter 8

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,26 @@
+module SessionsHelper
+  def log_in user
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+end
